48452faf3022a6876712a69dd281ea0fbdd46439
lapui <lapui@DESKTOP-8L23PR8>
2025-06-30 21:12:10 -0300

    Adicionar arquivos de projeto.

---
+++ ECommerce.API/Controllers/UsuariosController.cs
@@ -0,0 +1,60 @@
+﻿using ECommerce.API.Models;
+using ECommerce.API.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuariosController : ControllerBase
+    {
+        private IUsuarioRepository _repository;
+        public UsuariosController()
+        {
+            _repository = new UsuarioRepository();
+
+        }
+        [HttpGet]
+        public IActionResult Get()
+        { 
+            return Ok(_repository.Get());//200 do HTTP
+       
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id) {
+            var usuario = _repository.Get(id);
+            if (usuario != null)
+                return Ok(_repository.Get(id));//200 do HTTP
+            else
+                return NotFound(); //Error HTTP 404
+        }
+
+        [HttpPost]
+        public IActionResult Insert([FromBody] Usuario usuario) { 
+            _repository.Insert(usuario);//200 do HTTP
+            return Ok(usuario);
+        }
+
+        [HttpPut]
+        public IActionResult Update([FromBody] Usuario usuario)
+        {
+
+            _repository.Update(usuario);//200 do HTTP
+            return Ok(usuario);
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            _repository.Delete(id);//200 do HTTP
+            return Ok();
+
+        }
+
+
+
+
+    }
+}
---
+++ ECommerce.API/Models/Usuario.cs
@@ -0,0 +1,12 @@
+﻿namespace ECommerce.API.Models
+{
+    public class Usuario
+    {
+
+        public int Id { get; set; }
+        public String Nome { get; set; }
+        public String Email { get; set; }
+
+
+    }
+}
