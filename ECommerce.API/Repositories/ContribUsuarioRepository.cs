d3949c8ca402bd87e0a3f829a51b1a00169e5c3a
lapui <lapui@DESKTOP-8L23PR8>
2025-07-02 18:08:51 -0300

    Contrilb Dapper

---
+++ ECommerce.API/Repositories/ContribUsuarioRepository.cs
@@ -0,0 +1,33 @@
+﻿using ECommerce.API.Models;
+
+namespace ECommerce.API.Repositories
+{
+    public class ContribUsuarioRepository : IUsuarioRepository
+    {
+        public List<Usuario> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Usuario Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(Usuario usuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Usuario usuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
